628f3eb667b93ca4c432a4c899a44a56ad712386
OUAlieksieiev <taurusdreamer@gmail.com>
2026-01-26 23:27:45 +0100

    feat: add `RewardScreen` component to display different reward types and a restart button.

--- src/components/RewardScreen.jsx
+++ src/components/RewardScreen.jsx
@@ -17,12 +17,12 @@ const RewardScreen = ({ rewardType, onRestart }) => {
       {rewardType === 'diamond' && (
         <div className="coin-anim flex flex-col items-center">
           <img src={`${import.meta.env.BASE_URL}assets/image7-new.png`} alt="Diamond" className="w-full h-full md:w-100 md:h-100  drop-shadow-2xl mb-8" />
-          <h2 className="text-5xl font-bold text-slate-800 drop-shadow-md px-8 text-center">ТАК ТРИМАТИ! ДІАМАНТ!</h2>
+          <h2 className="text-5xl font-bold text-slate-800 drop-shadow-md px-8 text-center">ТАК ТРИМАТИ!</h2>
         </div>
       )}
       {rewardType === 'trophy' && (
         <div className="coin-anim flex flex-col items-center text-center">
-          <h2 className="text-6xl font-bold text-slate-800 drop-shadow-md mb-8 px-8">ВІТАЄМО ВАС! ВИГРАШ!</h2>
+          <h2 className="text-6xl font-bold text-slate-800 drop-shadow-md mb-8 px-8">ВІТАЄМО ВАС!</h2>
           <img src={`${import.meta.env.BASE_URL}assets/win_transparent-new.png`} alt="Trophy" className="w-full h-full md:w-100 drop-shadow-2xl mb-8" />
           <button
             onClick={onRestart}
