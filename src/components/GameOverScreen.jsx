2d42adc4336aa090a9e94d9e29fe5a03edae813f
OUAlieksieiev <taurusdreamer@gmail.com>
2026-01-26 21:27:37 +0100

    game over screen update

--- src/components/GameOverScreen.jsx
+++ src/components/GameOverScreen.jsx
@@ -7,14 +7,14 @@ const GameOverScreen = ({ onRestart }) => {
     <motion.div 
       key="gameover"
       initial={{ opacity: 0 }} animate={{ opacity: 1 }}
-      className="flex flex-col w-full bg-black/60 backdrop-blur-xl p-16 sm:mx-2 md:mx-4 lg:mx-6 max-w-[375px] mx-auto rounded-[2rem] border-8 border-slate-700 max-w-lg text-center"
+      className="flex flex-col w-full  flex-center items-center justify-center p-16 sm:mx-4 md:mx-4 lg:mx-6 mx-auto text-center"
     >
-       <RefreshCw size={100} className="text-slate-400 mb-8" />
-       <h2 className="text-5xl font-bold text-white mb-4">СПРОБУЙ ЩЕ</h2>
-       <p className="text-slate-300 mb-12 text-xl">Наберіть 3 діаманти за 60 запитань, щоб виграти!</p>
+       <RefreshCw size={100} className="text-black mb-8 mx-auto" />
+       <h2 className="text-5xl font-bold text-black mb-4">СПРОБУЙ ЩЕ</h2>
+       <p className="text-black mb-12 text-xl">Наберіть 3 діаманти за 60 запитань, щоб виграти!</p>
        <button 
         onClick={onRestart}
-        className="px-12 py-6 yellow-button text-3xl font-bold text-slate-900"
+        className="px-12 py-6 yellow-button text-3xl font-bold text-slate-900 text-center mx-auto"
       >
         НА ГОЛОВНУ
       </button>
